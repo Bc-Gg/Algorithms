02ffe264587c68a794cb85f3ede7808f8aa86a31
Bcgg <liubenchen18@163.com>
2022-03-24 20:17:04 +0800

    add new way to solve test4/4.cpp

--- code/test4/4.cpp
+++ code/test4/4.cpp
@@ -5,6 +5,8 @@
 巴大蝶  3
 小火龙 4
 */
+
+
 # include<iostream>
 # include<vector>
 
@@ -39,3 +41,59 @@ int main(){
     }
     return 0;
 }
+
+/*
+author ： jxgg
+
+#include<iostream>
+using namespace std;
+
+const unsigned long long N=1e9+7;
+
+unsigned long long a[5][5]={0,1,0,0,0,1,0,1,0,0,1,1,0,1,1,0,0,1,0,1,1,0,0,0,0};
+unsigned long long b[5][5]={0,1,0,0,0,1,0,1,0,0,1,1,0,1,1,0,0,1,0,1,1,0,0,0,0};
+unsigned long long c[5][5];
+int i,j,k;
+int nn;
+int t;
+unsigned long long sum=0;
+
+
+int main()
+{
+	cin>>t;
+	while(t--){
+		cin>>nn;
+		if(nn==1) cout<<"5"<<endl;
+		else if(nn==2) cout<<"10"<<endl;
+		else{
+		nn-=2;
+	while(nn--){
+	for(i = 0; i < 5; ++i)//矩阵相乘
+	{
+		for(j = 0; j < 5; ++j)
+		{
+			c[i][j]=0;
+			for(k = 0; k < 5; ++k)
+			c[i][j] += (a[i][k] * b[k][j]) % N; 
+		}	
+	}
+	for(int ii=0;ii<5;ii++)
+		for(int jj=0;jj<5;jj++) a[ii][jj]=c[ii][jj];
+	}
+    sum=0;
+	for(i = 0; i < 5; ++i)//输出相乘后的矩阵
+	{
+		for(j = 0; j < 5; ++j){
+            sum+=c[i][j] % N;
+        }
+	}
+    cout<<sum % N<<endl;
+	for(int ii=0;ii<5;ii++)
+		for(int jj=0;jj<5;jj++) a[ii][jj]=b[ii][jj];
+	}
+	}
+	return 0;
+ }
+
+*/
