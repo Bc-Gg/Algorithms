00de3107ec4b21d22314cddbed8c308c4994496a
Bcgg <liubenchen18@163.com>
2022-03-30 15:51:11 +0800

    upload 2.cpp

--- code/test5/2.cpp
+++ code/test5/2.cpp
@@ -1,23 +1,26 @@
 # include<iostream>
 #include<vector>
 using namespace std;
-const int N = 10010;
+const int N = 100100;
 
 int a[N];
-int f[N];
+int f1[N];
+int f2[N];
 int main(){
     int n;
     cin >> n;
     for (int i = 0 ; i < n ; i++) cin >> a[i];
-    f[0] = a[0];
-    f[1] = max(a[0], a[1]);
-    for(int i = 2; i < n ; i++){
-        if(i !=  n-1)
-            f[i] = max(f[i-1], f[i-2] + a[i]);
-        else 
-            f[i] = f[i-1];
+    if(n == 1){ cout << a[0]; return 0;}
+    f1[0] = f1[1] = a[0];
+    f2[0] = 0;
+    f2[1] = a[1];
+    for(int i = 2 ; i < n - 1; i++){// 选第一个
+        f1[i] = max(f1[i-1], f1[i-2] + a[i]);
     }
-    int ans = max(f[n-1],f[n-2]);
-    cout << ans;
+    f1[n-1] = f1[n-2];
+    for(int i = 2; i < n;i++){
+        f2[i] = max(f2[i-1],f2[i-2]+a[i]);
+    }
+    cout << max(f1[n-1],f2[n-1]);
     return 0;
 }
