c4811fd12faeed80902189c2d90c9abeb775f7cf
Bcgg <liubenchen18@163.com>
2022-04-27 17:32:23 +0800

    update

--- code/test8/4.cpp
+++ code/test8/4.cpp
@@ -1,64 +1,48 @@
-# include<iostream>
-# include<algorithm>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
-int n;
-int output[20];
-int vis[20];
-
-int check(int a){// yes->1   no->0
-    if(a%2 == 0) return 0;
-    int list[11] = {2,3,5,7,11,13,17,19,23,29,31};
-    for(int i = 0; i < 11 ; i++)
-        if(a == list[i]) return 1;
+const int N = 21, M = 35;
+int vis[N];
+int g[N];
+int list[] = {2,3,5,7,11,13,17,19,23,29,31};
+int is_prime(int a){
+    for(int i = 0 ; i < 11 ; i++)
+        if (a == list[i]) return 1;
     return 0;
 }
 
-
-void dfs(int pos){
-    if(pos == n+1 && check(1 + output[n]))
-    {
-        for(int i = 1 ; i <= n ; i++){
-            printf("%d ",output[i]);
+void dfs(int pos, int n){
+    if(pos == n + 1){
+        if(is_prime(1 + g[n])){
+            for(int i = 1; i <= n; i++){
+                printf("%d", g[i]);
+                if(i == n) puts("");
+                else printf(" ");
+            }
+            return;
         }
-        cout << endl;
     }
-    else{
-        if(output[pos-1]%2){
-            for(int i = 2 ; i <= n ; i+=2){
-                if(!vis[i] && check(i+output[pos-1])){
-                    output[pos] = i;
-                    vis[i] = 1;
-                    dfs(pos+1);
-                    vis[i] = 0;
-                }
-            }
-        }
-        else{
-            for(int i = 3 ; i <= n ; i+=2){
-                if(!vis[i] && check(i+output[pos-1])){
-                    output[pos] = i;
-                    vis[i] = 1;
-                    dfs(pos+1);
-                    vis[i] = 0;
-                }
-            }
+    for(int i = 2; i <= n; i++){
+        if(!vis[i] && is_prime(g[pos-1] + i)){
+            vis[i] = 1;
+            g[pos] = i;
+            dfs(pos+1, n);
+            vis[i] = 0;
         }
     }
 }
 
-
-int main(){
-    int cnt = 1;
-    while(scanf("%d",&n)!=EOF && n){
-        printf("Case %d:\n",cnt);
-        if(n%2==0){
-        output[1] = 1;
-        vis[1] = 1; 
-        dfs(2);
-        }
-        cout << endl;
+int main()
+{
+    g[1] = 1;
+    int n, cnt = 0;
+    while(scanf("%d", &n) != EOF && n){
         cnt++;
+        printf("Case %d:\n", cnt);
+        memset(vis, 0, sizeof vis);
+        if(n > 1) dfs(2, n);
+      	else printf("1\n");
+        puts("");
     }
     return 0;
 }
